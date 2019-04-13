ebd1e1682fd5c3d2cd3f9336b6d121825ea71fa3
jaml <lancasja@gmail.com>
2019-04-13 14:43:18 -0700

    moves loading script to end of body; navigates to final page on progress bar end

---
+++ js/loading.js
@@ -0,0 +1,5 @@
+const loadingBar = document.querySelector('.loader');
+
+loadingBar.addEventListener('animationend', function() {
+    window.location = window.location.origin + '/search-results.html';
+});
